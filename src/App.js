136c9cd0acccf3ac764cdd13d23349f57932abc4
kmavillanosa1 <kmavillanosa@gmail.com>
2020-06-12 19:07:09 +0800

    Added Initial Files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>Kim The Real G</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
